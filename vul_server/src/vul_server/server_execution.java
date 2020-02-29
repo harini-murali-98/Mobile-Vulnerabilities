d8982548a0a90ee9e1929437bb2a4192d4992f45
mharini06 <35478755+mharini06@users.noreply.github.com>
2020-03-01 00:31:06 +0530

    Add files via upload

--- vul_server/src/vul_server/server_execution.java
+++ vul_server/src/vul_server/server_execution.java
@@ -42,23 +42,35 @@ public class server_execution {
 				System.out.println(packName);
 				System.out.println(code);
 
-				switch (code) {
-				case "M1":
-
-					executeADB(packName);
-					break;
-				case "M2":
-					executeADB_encryption(packName);
-					break;
-				case "M3":
-					executeADB_dataleak(packName);
-					break;
-
-				case "M5":
-					executeADB_clipboardleak(packName);
-					break;
-
-				}
+//				switch (code) {
+//				case "M1":
+//
+//					executeADB(packName);
+//					break;
+//				case "M2":
+//					executeADB_encryption(packName);
+//					break;
+//				case "M3":
+//					executeADB_dataleak(packName);
+//					break;
+//
+//				case "M5":
+//					executeADB_clipboardleak(packName);
+//					break;
+//
+//				}
+				
+				String sharedPrefVul = executeADB(packName);
+				String localDBVul = executeADB_encryption(packName);
+				String logVul = executeADB_dataleak(packName);
+				String clipboardVul = executeADB_clipboardleak(packName);
+				
+				String serverResponse = sharedPrefVul+"\n"+localDBVul+"\n"+logVul+"\n"+clipboardVul;
+				
+				ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+				os.writeObject(serverResponse);
+				
+				
 
 			}
 
@@ -69,7 +81,7 @@ public class server_execution {
 	}
 
 	// vul-1 (shared prefs check)
-	public static void executeADB(String pack) throws IOException {
+	public static String executeADB(String pack) throws IOException {
 
 		ExecuteCommand ec = new ExecuteCommand();
 
@@ -90,26 +102,27 @@ public class server_execution {
 		}
 
 		if (spflag == 1) {
-			String messageS = "This App is Vulnerable due to exposed Shared Preferences File";
-			ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-			os.writeObject(messageS);
+			String messageS = "Yes";
+		
 
 			spflag = 0;
+			
+			return messageS;
 
 		} else {
-			String messageF = "This App is Not Vulnerable due to any exposed Shared Preferences File";
-			ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-			os.writeObject(messageF);
-
+			String messageF = "No";
+			
+     
+			return messageF;
 		}
 	}
 
 	// vul-2 (encrypted db check)
-	public static void executeADB_encryption(String pack) throws IOException {
+	public static String executeADB_encryption(String pack) throws IOException {
 		String message = "";
 		String localDb;
 		ExecuteCommand ec = new ExecuteCommand();
-		ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+		
 		String nameOfDBFiles = ec.command("adb shell run-as " + pack + " ls /data/data/" + pack + "/databases");
 		System.out.println("Name of database " + nameOfDBFiles);
 		String dbnames[] = nameOfDBFiles.split("\n");
@@ -127,17 +140,29 @@ public class server_execution {
 				ec.command("adb pull /data/data/" + pack + "/databases/" + db + "C:\\Users\\mhari\\DBFiles");
 
 				if (isValidSQLite("C:\\Users\\mhari\\DBfiles\\" + db)) {
-					os.writeObject("This App has unencrypted DataBase File");
+					System.out.println("db present but not excrypted");
+					message = "Yes";
+					
+					
+					
 				} else {
-					os.writeObject("This App has encrypted DataBase File");
-				}
+					System.out.println("db present and  excrypted");
+					message="No";
+
+					
+									}
 
 			}
 
 		} else {
-			System.out.println("The app doesn't store database in local storage");
-			os.writeObject("The app doesn't store database in local storage");
+			System.out.println("db not present");
+			message="No";
+		
+			
+			
+			
 		}
+		return message ;
 
 	}
 
@@ -166,41 +191,46 @@ public class server_execution {
 	}
 
 	// vu;-3(data leakage check)
-	public static void executeADB_dataleak(String pack) throws IOException {
-
+	public static String executeADB_dataleak(String pack) throws IOException {
+String message="";
 		ExecuteCommand ec = new ExecuteCommand();
-		ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+	
 		String pid = ec.command("adb shell pidof " + pack);
 		pid = pid.replaceAll("\\n", "");
 		ec.command("cmd /c adb logcat -d > C:\\Users\\mhari\\Desktop\\log_capture.txt");
 		File newFile = new File("C:\\\\Users\\\\mhari\\\\Desktop\\\\log_capture.txt");
 
 		if (newFile.length() == 0) {
+			
 			System.out.println("File is empty after creating it...");
 
-			os.writeObject("Log Contents not Leaked from App");
+			message = "No";
+			
 		} else {
 			System.out.println("File is not empty after creation...");
 
-			os.writeObject("Log Contents Leaked from App");
+			message = "Yes";
+			
 		}
-
+return message;
 	}
 
-	private static void executeADB_clipboardleak(String packName) throws IOException {
-
+	private static String executeADB_clipboardleak(String packName) throws IOException {
+String message="";
 		ExecuteCommand ec = new ExecuteCommand();
-		ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+		
 		String packages_with_clipboard = ec.command("adb shell cmd appops query-op --user 0 READ_CLIPBOARD allow");
 		if (packages_with_clipboard.contains(packName)) {
 			System.out.println("vul");
-			os.writeObject("Vulnerable");
+			message = "Yes";
+			
 
 		} else {
 			System.out.println("not");
-			os.writeObject("Not Vulnerable");
+			message = "No";
+			
 		}
-
+return message;
 	}
 
 }
